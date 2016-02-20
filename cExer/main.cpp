1bcfd4ef8dfa8d4f797d5857fb9ba42a57bc1d93
邰 晓梅 <taixiaomei@taideMacBook-Air.local>
2016-02-20 10:25:31 +0800

    Initial Commit

---
+++ cExer/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  cExer
+//
+//  Created by 邰 晓梅 on 16-2-20.
+//  Copyright (c) 2016年 邰 晓梅. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
